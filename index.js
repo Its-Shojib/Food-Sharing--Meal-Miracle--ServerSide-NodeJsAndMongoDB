9689a331bf3146b5dac21f48f5c8a96e32ceab95
Its_Shojib <mdshojib922@gmail.com>
2023-11-05 02:26:26 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port =process.env.PORT || 5000
+
+/* Using Middleware */
+app.use(cors());
+app.use(express.json());
+
+/* Starting MongoDB */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oglq0ui.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Food is Sharing!')
+})
+
+app.listen(port, () => {
+  console.log(`Food Sharing website is listening on port ${port}`)
+})
