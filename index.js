ecc8276e936e95b26216e5be7421a9f8e0ddf49c
Its_Shojib <mdshojib922@gmail.com>
2023-11-05 02:37:47 +0600

    Connected to the MongoDB

--- index.js
+++ index.js
@@ -24,8 +24,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const foodCollection = client.db("Meal-Miracle-DB").collection('foods');
+    
+    /*Insert Food Operation*/
+    app.post('foods', async(req,res)=>{
+        let newFood = req.body;
+        let result = await foodCollection.insertOne(newFood);
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
